dfc0ed56c9af234f7e0762e226e63acddf445d4a
EduardoLorenzetti <eduardoblorenzetti517@gmail.com>
2023-12-29 16:43:52 -0300

    CRUD produto finalizado

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FormRequestProduto;
+use App\Models\Componentes;
 use App\Models\Produto;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class ProdutosController extends Controller {
 
     public function index(Request $request) {
         $pesquisar = $request->pesquisar;
-        $findProduto = $this->produto->getProdutosPesquisarIndex(search: $pesquisar ?? 'd');
+        $findProduto = $this->produto->getProdutosPesquisarIndex(search: $pesquisar ?? '');
         return view('pages.produtos.paginacao', compact('findProduto'));
     }
 
@@ -26,9 +27,24 @@ class ProdutosController extends Controller {
     public function cadastrarProduto(FormRequestProduto $request) {
         if ($request->method() == "POST") {
             $data = $request->all();
+            $componentes = new Componentes();
+            $data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
             Produto::create($data);
             return redirect()->route('produto.index');
         }
         return view('pages.produtos.create');
+    }
+    public function atualizarProduto(FormRequestProduto $request, $id) {
+        if ($request->method() == "PUT") {
+            $data = $request->all();
+            $componentes = new Componentes();
+            $data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
+            $buscaRegistro = Produto::find($id);
+            $buscaRegistro->update($data);
+
+            return redirect()->route('produto.index');
+        }
+        $findProduto = Produto::where('id', '=', $id)->first();
+        return view('pages.produtos.atualiza', compact('findProduto'));
     }
 }
--- public/js/projeto.js
+++ public/js/projeto.js
@@ -1,28 +1,30 @@
-function deleteRegistroPaginacao(rotaUrl, idDoRegistro){
-    if(confirm('Deseja excluir')){
+function deleteRegistroPaginacao(rotaUrl, idDoRegistro) {
+    if (confirm('Deseja excluir')) {
         $.ajax({
             url: rotaUrl,
             method: 'DELETE',
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-            data:{
+            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            data: {
                 id: idDoRegistro,
             },
-            beforeSend: function (){
+            beforeSend: function () {
                 $.blockUI({
                     message: 'Carregando',
                     timeout: 2000,
                 });
             },
-        }).done(function (data){
+        }).done(function (data) {
             $.unblockUI();
-            if(data.success == true){
+            if (data.success == true) {
                 window.location.reload();
-            }else{
+            } else {
                 alert('Nao foi possivel excluir');
             }
-        }).fail(function(data){
+        }).fail(function (data) {
             $.unblockUI();
             alert('Nao foi possivel buscar dados');
         });
     }
 }
+
+$('#mascara_valor').mask('#.##0,00', { reverse: true })
