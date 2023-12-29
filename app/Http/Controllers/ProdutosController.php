9f8aa15f47d04d5fc998106fce6d9d5622a298ae
EduardoLorenzetti <eduardoblorenzetti517@gmail.com>
2023-12-29 15:35:02 -0300

    imports e criar produto

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -2,25 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FormRequestProduto;
 use App\Models\Produto;
 use Illuminate\Http\Request;
 
-class ProdutosController extends Controller
-{
-    public function __construct(Produto $produto){
+class ProdutosController extends Controller {
+    public function __construct(Produto $produto) {
         $this->produto = $produto;
     }
 
-    public function index(Request $request){
+    public function index(Request $request) {
         $pesquisar = $request->pesquisar;
         $findProduto = $this->produto->getProdutosPesquisarIndex(search: $pesquisar ?? 'd');
         return view('pages.produtos.paginacao', compact('findProduto'));
     }
 
-    public function delete(Request $request){
+    public function delete(Request $request) {
         $id = $request->id;
         $buscaRegistro = Produto::find($id);
         $buscaRegistro->delete();
         return response()->json(['success' => true]);
-    } 
+    }
+    public function cadastrarProduto(FormRequestProduto $request) {
+        if ($request->method() == "POST") {
+            $data = $request->all();
+            Produto::create($data);
+            return redirect()->route('produto.index');
+        }
+        return view('pages.produtos.create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::prefix('produtos')->group(function() {
-    Route::get('/',[ProdutosController::class, 'index'])->name('produto.index');
-    Route::delete('/delete',[ProdutosController::class, 'delete'])->name('produto.delete');
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProdutosController::class, 'index'])->name('produto.index');
+    Route::get('/cadastrarProduto', [ProdutosController::class, 'cadastrarProduto'])->name('cadastrar.produto');
+    Route::post('/cadastrarProduto', [ProdutosController::class, 'cadastrarProduto'])->name('cadastrar.produto');
+    Route::delete('/delete', [ProdutosController::class, 'delete'])->name('produto.delete');
 });
