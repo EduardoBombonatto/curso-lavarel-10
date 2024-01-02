fe776b162cb6b80b3b18595352301dc0e985281a
EduardoLorenzetti <eduardoblorenzetti517@gmail.com>
2024-01-02 15:56:49 -0300

    Crud vendas

---
+++ resources/views/pages/vendas/paginacao.blade.php
@@ -0,0 +1,47 @@
+{{-- extende da index --}}
+@extends('index')
+
+@section('content')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Vendas</h1>
+    </div>
+    <div>
+        <form action="{{ route('vendas.index') }}" method="get">
+            <input type="text" name="pesquisar" placeholder="Digite o nome" />
+            <button> Pesquisar </button>
+            <a type="button" href="{{ route('cadastrar.venda') }}" class="btn btn-success float-end">
+                Cadastrar venda
+            </a>
+        </form>
+        <div class="table-responsive mt-4">
+            @if ($findVendas->isEmpty())
+                <p>Nao existe vendas</p>
+            @else
+                <table class="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Numero da venda</th>
+                            <th>Produto</th>
+                            <th>Cliente</th>
+                            <th>Enviar email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($findVendas as $venda)
+                            <tr>
+                                <td>{{ $venda->numero_da_venda }}</td>
+                                <td>{{ $venda->produto->nome }}</td>
+                                <td>{{ $venda->cliente->nome }}</td>
+                                <td>
+                                    <a href="{{ route('atualizar.produto', $produto->id) }}" class="btn btn-light btn-sm">
+                                        Editar
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
+    </div>
+@endsection
